dde342bb7074c74628cd9ace9fd21b20bfe758d5
leah karienye <karienyeleah@gmail.com>
2022-06-07 21:21:05 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun (fun = "roller-skate") {
+
+    console.log(`This Saturday i want to ${fun}.`);
+  }
+console.log (saturdayFun());
+
+function mondayWork () {
+    const activity = "go to the office"
+    console.log(`This Monday, I will ${activity} .`);
+  }
+  function wrapAdjective () {
+    const part1 = "special";
+  return function () {
+    const part2 = "ideas";
+    return function () {
+      console.log(`you are ${part1()} where ${part2} `);
+    };
+  };
+}
+
+console.log (wrapAdjective)()();
